28aa497d3b78ad297d48f15b1b7adb5da20fa343
Itamar Syn-Hershko <itamar@code972.com>
2017-02-02 11:20:29 +0200

    Build and deploy script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+version=`cat package.json | grep version | cut -d: -f2 | sed -e "s/\"//g" | sed -e "s/ //g" | sed -e "s/\,//g"`
+
+functionName=LambdaStreamToEs
+filename=$functionName-$version.zip
+region=us-east-1
+
+npm install
+
+rm $filename 2>&1 >> /dev/null
+
+zip -x \*node_modules/protobufjs/tests/\* -r $filename index.js router.js transformer.js constants.js lambda.json package.json node_modules/ README.md LICENSE && mv -f $filename dist/$filename
+
+if [ ! -z "$1" ]; then
+    aws s3 cp dist/$filename $1/$filename
+fi
